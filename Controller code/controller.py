cf52a3880b129f573106522f9c84f95836d05cff
TjarleiChaplinn <58033515+TjarleiChaplinn@users.noreply.github.com>
2021-01-14 15:09:08 +0100

    Controller edited

--- Controller code/controller.py
+++ Controller code/controller.py
@@ -1,39 +1,19 @@
 import pygame
+import time
     
+pygame.init()
+pygame.joystick.init()
 controller = pygame.joystick.Joystick(0)
 controller.init()
 
-axis = {}
-button = {}
-
-
-    # these are the identifiers for the PS4's accelerometers
-AXIS_X = 3
-AXIS_Y = 4
-
-rot_x = 0.0
-rot_y = 0.0
-
-    # variables we'll store the rotations in, initialised to zero
-    
-    # main loop
 while True:
-
-        # copy rot_x/rot_y into axis[] in case we don't read any
-        axis[AXIS_X] = rot_x
-        axis[AXIS_Y] = rot_y
-
-        # retrieve any events ...
-        for event in pygame.event.get():
-
-            if event.type == pygame.JOYAXISMOTION:
-                axis[event.axis] = round(event.value,2)
-            elif event.type == pygame.JOYBUTTONDOWN:
-                button[event.button] = True
-            elif event.type == pygame.JOYBUTTONUP:
-                button[event.button] = False
-
-        rot_x = axis[AXIS_X]
-        rot_y = axis[AXIS_Y]            
-
-        # do something with this ...
+    for event in pygame.event.get():
+        if event.type == pygame.JOYBUTTONDOWN:
+            if controller.get_button(5):
+                print("R1")
+            elif controller.get_button(4):
+                print("L1")
+            elif controller.get_button(6):
+                print("L2")
+            elif controller.get_button(7):
+                print("R2")
